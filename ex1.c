7abde171c8e19967f53eece4cca273140340b6d6
rjcmarkelz <markelz@gmail.com>
2015-07-04 15:16:50 -0700

    exercises 1 to 3

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	puts("hello world.");
+	puts("hello world.");
+	puts("hello world.");
+	puts("hello world.");
+	puts("hello world.");
+	puts("hello world.");
+	
+	return 0;
+}
